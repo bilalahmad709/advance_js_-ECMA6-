9f0eaa59b37a86975b4ea39d2051156f58982679
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-11 16:25:26 +0500

    worked on var, let and constant in ECMA6

---
+++ ecmaVar.js
@@ -0,0 +1,22 @@
+document.write("<h2> Redeclaring/ using Of Variables, let and Constant</h2>");
+
+// variable (redeclaring, reassigningn of variable)
+
+document.write("<h3> A Variable can be redeclare & re Assign as well.</h3> <br>");
+document.write("<p>such as the below example, the value changed from 'Hello world ' to 'welcome to muy channel'</p>");
+var a= "Hello world";
+ a="welcome to my channel";
+document.write(a + "<br>");
+
+// let (redeclaring, reassigningn of let)
+document.write("<br><h3> A let value can not be redeclare but can be  Re-Assign.</h3>");
+let b = "Bilal Ahmad";
+ b ="Khan baba";
+document.write(b);
+
+// constant  (redeclaring, reassigningn of constant)
+document.write("<br><h3> A constant value can not be redeclare nor can be  Re-Assign.</h3>");
+const c = "its a contant value";
+document.write(c)
+
+document.write("<h4> Note : that if you will Re-assign the constant value then it will cause an error. </h4>");
