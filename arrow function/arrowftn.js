00696d8e7f6c4ddc2d9aca406a405ac301f4db37
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-13 20:52:45 +0500

    worked in arrow function

--- arrow function/arrowftn.js
+++ arrow function/arrowftn.js
@@ -2,8 +2,23 @@
 var arrowFtn=()=>{
    var arrow= document.getElementById("arrowftn");
    arrow.classList.add("arrowcss");
-   arrow.innerHTML="<h3>You have used the arrow function and applied the css class when you click the button.</h3>";
+   arrow.innerHTML="<h3>You have used the arrow function and applied the css class when you click the button.</h3> <br>";
+   document.write("<br>");
 
 }
-var arrow=document.getElementById("arrowftn").addEventListener("click", arrowFtn);
+var firstArrow=document.getElementById("arrowftn").addEventListener("click", arrowFtn);
 
+// second example of arrow function
+
+let secondArrowftn=()=>{
+   let secondFtn= document.getElementById("secondftn");
+   document.getElementById("secondftn").innerHTML="<p>this was the the second arrow function.</p>"
+   secondFtn.setAttribute("style","color=red; ")
+}
+var secondArrow= document.getElementById("secondftn").addEventListener("click", secondArrowftn);
+
+// third example of arrow finction
+
+const thirdArrowftn=()=>{
+   const thirdFtn= document.getElementById("")
+}
