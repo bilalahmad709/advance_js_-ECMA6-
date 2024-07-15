fa7284090e8255a343cff1fef984bae8551cb880
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-15 23:56:04 +0500

    worked in arrow function

--- arrow function/arrowftn.js
+++ arrow function/arrowftn.js
@@ -13,12 +13,15 @@ var firstArrow=document.getElementById("arrowftn").addEventListener("click", arr
 let secondArrowftn=()=>{
    let secondFtn= document.getElementById("secondftn");
    document.getElementById("secondftn").innerHTML="<p>this was the the second arrow function.</p>"
-   secondFtn.setAttribute("style","color=red; ")
+   secondFtn.setAttribute("style","color:red; ");
 }
 var secondArrow= document.getElementById("secondftn").addEventListener("click", secondArrowftn);
 
 // third example of arrow finction
 
 const thirdArrowftn=()=>{
-   const thirdFtn= document.getElementById("")
+   const thirdFtn= document.getElementById("thirdftn");
+   thirdFtn.setAttribute("style", "backgroundColor: gray; padding:10px;");
+
 }
+const thirdfunction = document.getElementById("thirdftn").addEventListener("click", thirdArrowftn );
