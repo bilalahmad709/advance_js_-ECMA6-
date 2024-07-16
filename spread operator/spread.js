2169193d5ac5a186de0cbfbb4c93f8efee8c2886
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-16 17:48:33 +0500

    worked in spread operator

---
+++ spread operator/spread.js
@@ -0,0 +1,14 @@
+document.write("<h2> What is spread Operator</h2>");
+
+// definition
+document.write("<p>The spread operator in JavaScript, represented by three dots (...), allows you to expand elements of an iterable (like an array or object) in places where zero or more arguments or elements are expected.</p>");
+
+
+// for example 
+
+//  function sum(x, y, z) {
+//   return x + y + z;
+// }
+// const numbers = [1, 2, 3];
+// console.log(sum(...numbers)); // 6
+
