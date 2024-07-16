d5e6e97233690fcd3011a0cbaf1b891c3cac7a49
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-16 16:29:41 +0500

    worked in rest operators

--- Rest Operators/restOperator.js
+++ Rest Operators/restOperator.js
@@ -8,3 +8,18 @@ function sum(){
 }
 sum(20,30,40);
 sum( 30, 40, 50,40)
+
+function summ(name,...args){
+
+document.write("<br>" +`Hello ${name} :`);
+let summ =0;
+for (let i in args){
+    summ += args[i];
+
+}
+document.write(summ + "<br> <br>");
+
+}
+summ("<br>" + "Bilal Ahmad the addtion of these values is  :", 20, 30, 40);
+
+summ("<br>" + "Waqar the addition of these values is :",30, 40,50,20);
