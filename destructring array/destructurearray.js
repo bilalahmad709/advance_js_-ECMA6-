e44ae29168da123ab3d5869a63de9f3515c783fc
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-22 18:41:24 +0500

    destructuring arrays

---
+++ destructring array/destructurearray.js
@@ -0,0 +1,9 @@
+document.write("Destructuring Array <br>");
+let intro=["Bilal Ahmad", 23, "Computer Science" , "Mohmand"];
+
+let [name, age, education, district]=intro;
+
+document.write(" <br>My name is : " + name);
+document.write("<br>My age is : "  + age);
+document.write("<br>I am a " + education + "Graduate");
+document.write("<br>I am from district :" + district);
