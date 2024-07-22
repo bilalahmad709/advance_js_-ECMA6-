7be314ffa76462318a1b108161334aa88a2381a3
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-22 17:03:09 +0500

    object literals

--- object literals/objLiterals.js
+++ object literals/objLiterals.js
@@ -44,3 +44,41 @@ document.write("Name" + Name + "<br>");
 document.write("Class : " + Class + "<br>");
 document.write("University :" + University + "<br>");
 console.log(obj);
+
+// second way of using object literals
+
+document.write(" <br> <h3>Use Variable as a property name </h3>");
+let n= "student";
+
+var obj= {
+    [n + "name"] : "Bilal Ahmad",
+    course : "Computer Science",
+    detail : function(){
+        return `${this.studentname} is the student of ${this.course}`
+    }
+};
+
+console.log(obj);
+console.log(obj.detail);
+document.write(obj.detail());
+
+// new function syntax
+
+document.write("<br> New Function Syntax in Object Literals <br>");
+
+// old method
+let obj1={
+    Name: "Zahid Khttak",
+    show : function(){
+        console.log(this.Name);
+    }
+}
+
+// new method
+
+let obj2={
+    Name : "Hilal Azam Binori",
+    show(){
+        console.log(this.Name);
+    }
+}
